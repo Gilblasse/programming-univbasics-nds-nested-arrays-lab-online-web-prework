3e2e693a547db120900b314b2e6bc746c6801cc1
Nethelbert <gilblasse@gmail.com>
2019-10-29 22:55:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,8 +26,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  binding.pry
-  # matrix[row][column]
+  mmatrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
